c4c7b59989b70f451f9eed3ff36d5f30b79f4634
JeanCarlosDelai <Jean@vieceli@outlook.com>
2023-09-12 11:14:27 -0300

    ✨ feat: Add Show Area Use Case

---
+++ src/modules/area/useCases/ShowAreaUseCase.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from 'tsyringe';
+import { IAreaRepository } from '../domain/repositories/IAreaRepository';
+import CustomAPIError from '@shared/errors';
+import { IArea } from '../domain/models/IArea';
+import { IShowArea } from '../domain/models/IShowArea';
+
+@injectable()
+class ShowAreaUseCase {
+  constructor(
+    @inject('AreaRepository')
+    private areasRepository: IAreaRepository,
+  ) {
+    if (!areasRepository) {
+      throw new Error('AreaRepository is required.');
+    }
+  }
+
+  public async execute({
+    property_id,
+    area_id,
+  }: IShowArea): Promise<IArea | null> {
+    const area = await this.areasRepository.findByIds(property_id, area_id);
+    if (!area) {
+      throw new CustomAPIError.BadRequestError('Property or Area not exist.');
+    }
+
+    return area;
+  }
+}
+
+export default ShowAreaUseCase;
