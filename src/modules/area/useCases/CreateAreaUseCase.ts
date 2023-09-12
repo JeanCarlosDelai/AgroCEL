8894e00ac49cd596f8a32b369117036a6c3d2ff8
JeanCarlosDelai <Jean@vieceli@outlook.com>
2023-09-12 11:12:36 -0300

    ✨ feat: Add Create Area Use Case

---
+++ src/modules/area/useCases/CreateAreaUseCase.ts
@@ -0,0 +1,59 @@
+import CustomAPIError from '@shared/errors';
+import { inject, injectable } from 'tsyringe';
+import { IAreaRepository } from '../domain/repositories/IAreaRepository';
+import { ICreateArea } from '../domain/models/ICreateArea';
+import { IArea } from '../domain/models/IArea';
+
+@injectable()
+class CreateAreaUseCase {
+  constructor(
+    @inject('AreaRepository')
+    private areaRepository: IAreaRepository,
+  ) {
+    if (!areaRepository) {
+      throw new Error('areaRepository is required.');
+    }
+  }
+
+  public async execute({
+    name,
+    property_id,
+    species,
+    variety,
+    driving_system,
+    rookstock_type,
+    cultivated_area,
+    geographic_coordinates,
+    implementation_date,
+    number_rows,
+    distance_between_rows,
+    distance_between_plants,
+    number_plants,
+  }: ICreateArea): Promise<IArea> {
+    const areaExists = await this.areaRepository.findByName(name, property_id);
+
+    if (areaExists) {
+      throw new CustomAPIError.BadRequestError('Area name already used.');
+    }
+
+    const area = await this.areaRepository.create({
+      name,
+      property_id,
+      species,
+      variety,
+      driving_system,
+      rookstock_type,
+      cultivated_area,
+      geographic_coordinates,
+      implementation_date,
+      number_rows,
+      distance_between_rows,
+      distance_between_plants,
+      number_plants,
+    });
+
+    return area;
+  }
+}
+
+export default CreateAreaUseCase;
