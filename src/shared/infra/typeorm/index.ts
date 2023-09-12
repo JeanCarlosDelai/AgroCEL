9e210b7d47e68854a3abd5a4f96c8352ef4a57ec
JeanCarlosDelai <Jean@vieceli@outlook.com>
2023-09-12 09:51:23 -0300

    ✨ feat: Add Migration Create Property Table

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,6 +2,7 @@ import User from '@modules/users/infra/typeorm/entities/User';
 import { DataSource } from 'typeorm';
 import { CreateUsers1607534203339 } from './migrations/1607534203339-CreateUsers';
 import { CreateUserTokens1607917238905 } from './migrations/1607917238905-CreateUserTokens';
+import { CreateProperty1608058533060 } from './migrations/1608058533060-CreateProperty';
 
 export const dataSource = new DataSource({
   type: 'postgres',
@@ -14,5 +15,7 @@ export const dataSource = new DataSource({
   migrations: [
     CreateUsers1607534203339,
     CreateUserTokens1607917238905,
+    CreateProperty1608058533060,
+
   ],
 });
