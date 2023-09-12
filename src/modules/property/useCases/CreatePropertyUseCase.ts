38dd12f687b7941314aeebf6ad92402018a9ac59
JeanCarlosDelai <Jean@vieceli@outlook.com>
2023-09-12 10:25:35 -0300

    ✨ feat: Create Property Use Case

---
+++ src/modules/property/useCases/CreatePropertyUseCase.ts
@@ -0,0 +1,48 @@
+import CustomAPIError from '@shared/errors';
+import { inject, injectable } from 'tsyringe';
+import { ICreateProperty } from '../domain/models/ICreateProperty';
+import { IProperty } from '../domain/models/IProperty';
+import { IPropertyRepository } from '../domain/repositories/IPropertyRepository';
+
+@injectable()
+class CreatePropertyUseCase {
+  constructor(
+    @inject('PropertyRepository')
+    private propertyRepository: IPropertyRepository,
+  ) {
+    if (!propertyRepository) {
+      throw new Error('PropertyRepository is required.');
+    }
+  }
+
+  public async execute({
+    name,
+    user_id,
+    total_area,
+    cultivated_area,
+    city,
+    state,
+  }: ICreateProperty): Promise<IProperty> {
+    const propertyExists = await this.propertyRepository.findByName(
+      name,
+      user_id,
+    );
+
+    if (propertyExists) {
+      throw new CustomAPIError.BadRequestError('Property name already used.');
+    }
+
+    const property = await this.propertyRepository.create({
+      name,
+      user_id,
+      total_area,
+      cultivated_area,
+      city,
+      state,
+    });
+
+    return property;
+  }
+}
+
+export default CreatePropertyUseCase;
