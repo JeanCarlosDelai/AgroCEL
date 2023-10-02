25d57cfedca36c2bb8da7ca66cf58e3f7f946232
JeanCarlosDelai <Jean@vieceli@outlook.com>
2023-10-02 11:02:43 -0300

    ✨ feat: Add CreateAreaUseCase tests

---
+++ src/modules/area/useCases/CreateAreaUseCase.spec.ts
@@ -0,0 +1,110 @@
+import { ICreateUser } from '@modules/user/domain/models/ICreateUser';
+import { IUser } from '@modules/user/domain/models/IUser';
+import { IUsersRepository } from '@modules/user/domain/repositories/IUsersRepository';
+import UsersRepositoryInMemory from '@modules/user/domain/repositories/in-memory/UsersRepositoryInMemory';
+import FakeHashProvider from '@modules/user/providers/HashProvider/fakes/FakeHashProvider';
+import { IHashProvider } from '@modules/user/providers/HashProvider/models/IHashPovider';
+import CreateUserUseCase from '@modules/user/useCases/CreateUserUseCase';
+import { ICreateArea } from '../domain/models/ICreateArea';
+import CreateAreaUseCase from './CreateAreaUseCase';
+import AreaRepositoryInMemory from '../domain/repositories/in-memory/AreaRepositoryInMemory';
+import { IAreaRepository } from '../domain/repositories/IAreaRepository';
+import { IArea } from '../domain/models/IArea';
+import PropertysRepositoryInMemory from '@modules/property/domain/repositories/in-memory/PropertysRepositoryInMemory';
+import CreatePropertyUseCase from '@modules/property/useCases/CreatePropertyUseCase';
+import { ICreateProperty } from '@modules/property/domain/models/ICreateProperty';
+import { IProperty } from '@modules/property/domain/models/IProperty';
+import { IPropertyRepository } from '@modules/property/domain/repositories/IPropertyRepository';
+
+let usersRepositoryInMemory: IUsersRepository;
+let propertysRepositoryInMemory: IPropertyRepository;
+let areasRepositoryInMemory: IAreaRepository;
+let fakehashedProvider: IHashProvider;
+let createUserUseCase: CreateUserUseCase;
+let createPropertyUseCase: CreatePropertyUseCase;
+let createAreaUseCase: CreateAreaUseCase;
+let user: IUser;
+let property: IProperty;
+let area: IArea;
+
+beforeAll(() => {
+  usersRepositoryInMemory = new UsersRepositoryInMemory();
+  propertysRepositoryInMemory = new PropertysRepositoryInMemory();
+  areasRepositoryInMemory = new AreaRepositoryInMemory();
+  fakehashedProvider = new FakeHashProvider();
+  createUserUseCase = new CreateUserUseCase(
+    usersRepositoryInMemory,
+    fakehashedProvider,
+  );
+  createPropertyUseCase = new CreatePropertyUseCase(
+    propertysRepositoryInMemory,
+  );
+  createAreaUseCase = new CreateAreaUseCase(areasRepositoryInMemory);
+});
+
+describe('Create Area', () => {
+  beforeAll(async () => {
+    const userData: ICreateUser = {
+      name: 'Jean teste',
+      email: 'teste@gmail.com',
+      password: await fakehashedProvider.generateHash('jean123'),
+    };
+    user = await createUserUseCase.execute(userData);
+
+    const propertyData: ICreateProperty = {
+      name: 'Propriedade 1',
+      user_id: user.id,
+      total_area: 10,
+      cultivated_area: 5,
+      city: 'Coronel Pilar',
+      state: 'RS',
+    };
+    property = await createPropertyUseCase.execute(propertyData);
+  });
+
+  it('Should be able to create a new area', async () => {
+    const areaData: ICreateArea = {
+      name: 'Pareiral Jackes3',
+      property_id: property.id,
+      species: 'Uva',
+      variety: 'Jackes',
+      driving_system: 'Latada',
+      rookstock_type: 'Pé Franco',
+      cultivated_area: 5,
+      geographic_coordinates: '123.3456.6565',
+      implementation_date: new Date(),
+      number_rows: 50,
+      distance_between_rows: 2,
+      distance_between_plants: 1,
+      number_plants: 2000,
+    };
+    area = await createAreaUseCase.execute(areaData);
+
+    expect(area).toHaveProperty('id');
+    expect(area.property_id).toBe(areaData.property_id);
+  });
+
+  it('Should not be able to create an existing area ', async () => {
+    const areaData: ICreateArea = {
+      name: 'Pareiral Jackes3',
+      property_id: property.id,
+      species: 'Uva',
+      variety: 'Jackes',
+      driving_system: 'Latada',
+      rookstock_type: 'Pé Franco',
+      cultivated_area: 5,
+      geographic_coordinates: '123.3456.6565',
+      implementation_date: new Date(),
+      number_rows: 50,
+      distance_between_rows: 2,
+      distance_between_plants: 1,
+      number_plants: 2000,
+    };
+
+    const expectErrorResponse = new Error('Area name already used.');
+
+    expect(createAreaUseCase.execute(areaData)).rejects.toThrowError(
+      expectErrorResponse,
+    );
+  });
+});
