4e0518b3c98177bc9f835119098d233c4c539f6b
JeanCarlosDelai <Jean@vieceli@outlook.com>
2023-10-04 12:57:18 -0300

    ✨ feat: Add ShowApplicationUseCase

---
+++ src/modules/application/useCases/ShowApplicationUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from 'tsyringe';
+import CustomAPIError from '@shared/errors';
+import { IApplicationRepository } from '../domain/repositories/IApplicationRepository';
+import { IShowApplication } from '../domain/models/IShowApplication';
+import { IApplication } from '../domain/models/IApplication';
+
+@injectable()
+class ShowApplicationUseCase {
+  constructor(
+    @inject('ApplicationRepository')
+    private aplicationRepository: IApplicationRepository, // eslint-disable-next-line prettier/prettier
+  ) // eslint-disable-next-line prettier/prettier
+  { }
+
+  public async execute({
+    id,
+    area_id,
+  }: IShowApplication): Promise<IApplication | null> {
+    const application = await this.aplicationRepository.findByIds(id, area_id);
+    if (!application) {
+      throw new CustomAPIError.BadRequestError(
+        'Area or Application not exist.',
+      );
+    }
+
+    return application;
+  }
+}
+
+export default ShowApplicationUseCase;
