184e71c8b04bf49fcb2f0cf46903e9a6573ce30b
JeanCarlosDelai <Jean@vieceli@outlook.com>
2023-10-05 16:04:35 -0300

    ✨ feat: Add UpdateCropSaleUseCase

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,7 @@ const config = {
     '^@modules/(.*)$': '<rootDir>/src/modules/$1',
     '^@shared/(.*)$': '<rootDir>/src/shared/$1',
   },
-  testMatch: ['**/**/*.test.ts'],
+  testMatch: ['**/**/*.spec.ts'],
 };
 
 module.exports = config;
