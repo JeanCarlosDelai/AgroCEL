c17cdd3d093b5eb1e6a795c0b8ff004771c08afb
JeanCarlosDelai <Jean@vieceli@outlook.com>
2023-11-02 18:16:19 -0300

    ♻️ refactor: Add Register Screen

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,23 +1,18 @@
 import main from '../assets/images/main.svg';
-import Wrapper from '../assets/wrappers/LandingPage';
-import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+
 const Landing = () => {
   return (
     <>
       <Navbar />
-      <Wrapper>
-        <div className="container page">
-          {/* info */}
-          <div className="info">
-            <h4>Caderno de campo digital</h4>
-            <Link to="/register" className="btn btn-hero">
-              Login/Registrar-se
-            </Link>
-          </div>
-          <img src={main} alt="job hunt" className="img main-img" />
+      <div className="flex items-center ">
+        <div className="flex ml-40">
+          <h1>Caderno de campo digital</h1>
         </div>
-      </Wrapper>
+        <div>
+          <img src={main} alt="job hunt" className="ml-40" />
+        </div>
+      </div>
     </>
   );
 };
---
+++ client/src/queries/users/users.jsx
@@ -0,0 +1,50 @@
+import { useQuery, useMutation } from 'react-query';
+import customFetch from '../../utils/axios';
+import { toast } from 'react-toastify';
+import queryClient from '../../services/queryClient';
+
+// async function getPropertys() {
+//   const { data, error } = await customFetch.get(`/property/user`);
+//   if (error) {
+//     toast.success(error);
+//   }
+//   return data.data;
+// }
+
+// export function useFetchProperty() {
+//   return useQuery(['propertys'], getPropertys);
+// }
+
+export const createUser = async (user) => {
+  try {
+    await customFetch.post('/users', user);
+    // await queryClient.invalidateQueries('propertys');
+    toast.success('Usuário criado com sucesso!');
+  } catch (error) {
+    toast.error(error.response.data.msg);
+  }
+};
+
+// export const updateProperty = async (property_id, property) => {
+//   try {
+//     const response = await customFetch.put(
+//       `/property/${property_id}`,
+//       property,
+//     );
+//     await queryClient.invalidateQueries('propertys');
+//     toast.success('Propriedade alterada com sucesso!');
+//     return response.data;
+//   } catch (error) {
+//     toast.error(error.response.data.msg);
+//   }
+// };
+
+// export const deleteProperty = async (property_id) => {
+//   try {
+//     await customFetch.delete(`/property/${property_id}`);
+//     await queryClient.invalidateQueries('propertys');
+//     toast.success('Propriedade apagada com sucesso!');
+//   } catch (error) {
+//     toast.error(error.response.data.msg);
+//   }
+// };
